0a8eda2cc86d94e51b1778b63da0d822fc0febd8
scottyvg <svangilder@gmail.com>
2017-03-01 11:23:48 -0700

    add git ignore

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var express = require('express');
 
 // createServer takes a callback function with a request and response object in it
-http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   });
+//   res.end('Hello World\n');
+// }).listen(3000, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000/');
+// console.log('Server running at http://127.0.0.1:3000/');
